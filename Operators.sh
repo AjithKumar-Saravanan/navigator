130ce37fac0896e8184d0d20179873a30f1f7576
AjithKumar-Saravanan <p.s.ajithkumar051@gmail.com>
2022-07-09 15:21:28 +0530

    Sh_File

---
+++ Operators.sh
@@ -0,0 +1,214 @@
+#Arithmetic Operator
+echo ""
+echo "Arithmetic Operator"
+val=`expr 2 + 2`
+echo "add value : $val"
+
+val=`expr 4 - 2`
+echo "sub value : $val"
+
+val=`expr 2 \* 2`
+echo "mul value : $val"
+
+val=`expr 8 / 2`
+echo "div value : $val"
+
+val=`expr 80 % 1000`
+echo "modulas value : $val"
+
+val=`expr a=2`
+echo "assign value : $val"
+
+val=`expr 2 == 2`
+echo "equality value : $val"
+
+val=`expr 2 != 2`
+echo "not equality value : $val"
+
+
+#Relational Operator
+echo ""
+echo "Relational Operator"
+a=10
+b=20
+
+if [ $a -eq $b ]
+then
+   echo "$a -eq $b : a is equal to b"
+else
+   echo "$a -eq $b: a is not equal to b"
+fi
+
+if [ $a -ne $b ]
+then
+   echo "$a -ne $b: a is not equal to b"
+else
+   echo "$a -ne $b : a is equal to b"
+fi
+
+if [ $a -gt $b ]
+then
+   echo "$a -gt $b: a is greater than b"
+else
+   echo "$a -gt $b: a is not greater than b"
+fi
+
+if [ $a -lt $b ]
+then
+   echo "$a -lt $b: a is less than b"
+else
+   echo "$a -lt $b: a is not less than b"
+fi
+
+if [ $a -ge $b ]
+then
+   echo "$a -ge $b: a is greater or  equal to b"
+else
+   echo "$a -ge $b: a is not greater or equal to b"
+fi
+
+if [ $a -le $b ]
+then
+   echo "$a -le $b: a is less or  equal to b"
+else
+   echo "$a -le $b: a is not less or equal to b"
+fi
+
+
+#boolean operator
+echo ""
+echo "Boolean Operator"
+a=10
+b=20
+
+if [ $a != $b ]
+then
+   echo "$a != $b : a is not equal to b"
+else
+   echo "$a != $b: a is equal to b"
+fi
+
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+   echo "$a -lt 100 -a $b -gt 15 : returns true"
+else
+   echo "$a -lt 100 -a $b -gt 15 : returns false"
+fi
+
+if [ $a -lt 5 -a $b -gt 15 ]
+then
+   echo "$a -lt 5 -a $b -gt 15 : returns true"
+else
+   echo "$a -lt 5 -a $b -gt 15 : returns false"
+fi
+
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+   echo "$a -lt 100 -o $b -gt 100 : returns true"
+else
+   echo "$a -lt 100 -o $b -gt 100 : returns false"
+fi
+
+if [ $a -lt 5 -o $b -gt 100 ]
+then
+   echo "$a -lt 5 -o $b -gt 100 : returns true"
+else
+   echo "$a -lt 5 -o $b -gt 100 : returns false"
+fi
+
+
+#String Operator
+echo ""
+echo "String Operator"
+a="abc"
+b="efg"
+
+if [ $a = $b ]
+then
+   echo "$a = $b : a is equal to b"
+else
+   echo "$a = $b: a is not equal to b"
+fi
+
+if [ $a != $b ]
+then
+   echo "$a != $b : a is not equal to b"
+else
+   echo "$a != $b: a is equal to b"
+fi
+
+if [ -z $a ]
+then
+   echo "-z $a : string length is zero"
+else
+   echo "-z $a : string length is not zero"
+fi
+
+if [ -n $a ]
+then
+   echo "-n $a : string length is not zero"
+else
+   echo "-n $a : string length is zero"
+fi
+
+if [ $a ]
+then
+   echo "$a : string is not empty"
+else
+   echo "$a : string is empty"
+fi
+
+
+#File Operator
+echo ""
+echo "File Operator"
+file="/home/e1025/script/Operators.sh"
+
+if [ -r $file ]
+then
+   echo "File has read access"
+else
+   echo "File does not have read access"
+fi
+
+if [ -w $file ]
+then
+   echo "File has write permission"
+else
+   echo "File does not have write permission"
+fi
+
+if [ -x $file ]
+then
+   echo "File has execute permission"
+else
+   echo "File does not have execute permission"
+fi
+
+if [ -f $file ]
+then
+   echo "File is an ordinary file"
+else
+   echo "This is sepcial file"
+fi
+
+if [ -d $file ]
+then
+   echo "File is a directory"
+else
+   echo "This is not a directory"
+fi
+
+if [ -s $file ]
+then
+   echo "File size is not zero"
+else
+   echo "File size is zero"
+fi
+
+if [ -e $file ]
+then
+   echo "File exists"
+else
+   echo "File does not exist"
+fi
